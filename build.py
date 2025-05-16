9c49b516aa95e07724fd73292aed06d89244576a
Vilmokas <vilmutizzz@gmail.com>
2025-05-16 17:10:24 +0300

    feat!: change website to template and data file
    will not need to edit html directly to add or remove projects, can just edit the json file

---
+++ build.py
@@ -0,0 +1,27 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+
+datafile = "source/data.json"
+templatefile = "source/index.template.html"
+outputfile = "index.html"
+
+# Load JSON data
+print("loading json data file...")
+with open(datafile) as f:
+    data = json.load(f)
+
+# Load Jinja2 template
+print("loading html template file...")
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template(templatefile)
+
+# Render template with data
+print("rendering template with data...")
+rendered_html = template.render(data)
+
+# Write to output file
+print("Writing to output file...")
+with open(outputfile, 'w', encoding='utf-8') as f:
+    f.write(rendered_html)
+
+print("✅ HTML file generated: " + outputfile)
